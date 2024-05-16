dfd7a9140fc96bdc93f3f7e1c3bf57d2cb8894dc
Theia <168746713+TheiaVasyukova@users.noreply.github.com>
2024-05-16 21:24:49 +0300

    add new result line

--- radius.py
+++ radius.py
@@ -9,3 +9,4 @@ radius = float(input("Введите радиус круга: "))
 area = calculate_circle_area(radius)
 
 print(f"Площадь круга: {area:.2f}")
+print(f"Площадь круга с радиусом {radius} равна {area:.2f}")
